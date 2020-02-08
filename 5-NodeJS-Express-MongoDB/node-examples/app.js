91c0041376fd47f1cb4889f4afdc412f726f0654
Ryan White <mrwhiterk@gmail.com>
2020-02-07 21:55:16 -0500

    added node week1 area module

---
+++ 5-NodeJS-Express-MongoDB/node-examples/app.js
@@ -0,0 +1,24 @@
+const rect = {
+  perimeter: (x, y) => {
+    return 2 * (x + y)
+  },
+  area: (x, y) => {
+    return x * y
+  }
+}
+
+function solveRect(l, w) {
+  console.log(`Solving for rect with dimensions: ${l}, ${w}`)
+
+  if (l <= 0 || w <= 0) {
+    console.log(`rect dimens must be > 0. received: ${l}, ${w}`)
+  } else {
+    console.log(`Area of rect: ${rect.area(l, w)}`)
+    console.log(`Perimeter of rect: ${rect.perimeter(l, w)}`)
+  }
+}
+
+solveRect(2, 4)
+solveRect(3, 5)
+solveRect(0, 5)
+solveRect(5, -3)
