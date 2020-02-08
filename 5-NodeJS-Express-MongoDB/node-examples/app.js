a3b2b6a1fe32446692bb085e82b8e67941491b9b
Ryan White <mrwhiterk@gmail.com>
2020-02-08 12:49:03 -0500

    exer4: callbacks and error handling in node

--- 5-NodeJS-Express-MongoDB/node-examples/app.js
+++ 5-NodeJS-Express-MongoDB/node-examples/app.js
@@ -1,23 +1,22 @@
-// const rect = {
-//   perimeter: (x, y) => {
-//     return 2 * (x + y)
-//   },
-//   area: (x, y) => {
-//     return x * y
-//   }
-// }
-
 const rect = require('./rectangle')
 
 function solveRect(l, w) {
   console.log(`Solving for rect with dimensions: ${l}, ${w}`)
 
-  if (l <= 0 || w <= 0) {
-    console.log(`rect dimens must be > 0. received: ${l}, ${w}`)
-  } else {
-    console.log(`Area of rect: ${rect.area(l, w)}`)
-    console.log(`Perimeter of rect: ${rect.perimeter(l, w)}`)
-  }
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log('ERROR: ', err.message)
+    } else {
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
+      )
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
+      )
+    }
+  })
+
+  console.log('====================================')
 }
 
 solveRect(2, 4)
