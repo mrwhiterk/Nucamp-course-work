6f146a88bc16aa958616cc3f9e3a111948d6883a
Ryan White <mrwhiterk@gmail.com>
2020-02-09 20:16:00 -0500

    week1 workshop task 1

--- 5-NodeJS-Express-MongoDB/node-express/routes/campsiteRouter.js
+++ 5-NodeJS-Express-MongoDB/node-express/routes/campsiteRouter.js
@@ -10,7 +10,7 @@ campsiteRouter
     next()
   })
   .get((req, res) => {
-    res.send('Will send all the campsites to you')
+    res.send('Will send all the campsites to you in timen')
   })
   .post((req, res) => {
     res.send(
@@ -26,6 +26,42 @@ campsiteRouter
   })
   .delete((req, res) => {
     res.end('deleting all campsites')
+  })
+
+campsiteRouter
+  .route('/:campsiteId')
+  .all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+  })
+  .get((req, res) => {
+    console.log('hit')
+    res.send('Will send all the campsites to you id: ' + req.params.campsiteId)
+  })
+  .post((req, res) => {
+    res.send(
+      'will add the campsite: ' +
+        req.body.name +
+        ' with desc: ' +
+        req.body.description +
+        ' id: ' +
+        req.params.campsiteId
+    )
+  })
+  .put((req, res) => {
+    res.statusCode = 403
+    res.send(
+      'will update the campsite: ' +
+        req.body.name +
+        ' with desc: ' +
+        req.body.description +
+        ' id: ' +
+        req.params.campsiteId
+    )
+  })
+  .delete((req, res) => {
+    res.end('deleting campsite with id: ' + req.params.campsiteId)
   })
 
 module.exports = campsiteRouter
