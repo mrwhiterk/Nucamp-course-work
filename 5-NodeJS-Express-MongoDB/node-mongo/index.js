b8d3f8c16e3a8d97e4ef4a473f54a3cf9e3feb28
Ryan White <mrwhiterk@gmail.com>
2020-02-17 22:12:46 -0500

    w2-3. Exercise: Node and MongoDB Part 2

--- 5-NodeJS-Express-MongoDB/node-mongo/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert').strict
+const dboper = require('./operations')
 const url = 'mongodb://localhost:27017/'
 const dbname = 'nucampsite'
 
@@ -12,22 +13,40 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
   db.dropCollection('campsites', (err, result) => {
     assert.strictEqual(err, null)
-
     console.log('dropped collection', result)
 
-    const collection = db.collection('campsites')
-
-    collection.insertOne(
-      { name: 'breadcrumb trail camp', description: 'test' },
-      (err, result) => {
-        assert.strictEqual(err, null)
+    dboper.insertDocument(
+      db,
+      { name: 'breadcrumb trail campground', description: 'test' },
+      'campsites',
+      result => {
         console.log('insert document: ', result.ops)
 
-        collection.find().toArray((err, docs) => {
-          assert.strictEqual(err, null)
-          console.log('found documents: ', docs)
+        dboper.findDocuments(db, 'campsites', docs => {
+          console.log('found docs: ', docs)
 
-          client.close()
+          dboper.updateDocument(
+            db,
+            { name: 'breadcrumb trail campground' },
+            { description: 'updated test description' },
+            'campsites',
+            result => {
+              console.log('updated document count: ', result.result.nModified)
+              dboper.findDocuments(db, 'campsites', docs => {
+                console.log('found documents: ', docs)
+
+                dboper.removeDocument(
+                  db,
+                  { name: 'breadcrumb trail campground' },
+                  'campsites',
+                  result => {
+                    console.log('deleted document count: ', result.deletedCount)
+                    client.close()
+                  }
+                )
+              })
+            }
+          )
         })
       }
     )
