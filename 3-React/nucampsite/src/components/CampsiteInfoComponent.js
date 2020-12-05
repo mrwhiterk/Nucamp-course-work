8c8ecbc149ce92bd478a216e5134b0e59d36f69c
Ryan White <mrwhiterk@gmail.com>
2020-12-04 21:55:08 -0500

    Finished React Week 4 workshop react-redux-form

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -58,12 +58,20 @@ function RenderComments({ comments, campsite }) {
   } else return <div />;
 }
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(+val);
+
 class CommentForm extends React.Component {
   state = {
     isModalOpen: false,
     rating: 0,
     text: "",
     author: "",
+    touched: {
+      author: false
+    }
   };
 
   toggleModal = () => {
@@ -110,6 +118,22 @@ class CommentForm extends React.Component {
               name="author"
               placeholder="author"
               className="form-control"
+              validators={{
+                required,
+                minLength: minLength(2),
+                maxLength: maxLength(15),
+              }}
+            />
+            <Errors
+              className="text-danger"
+              model=".author"
+              show="touched"
+              component="div"
+              messages={{
+                required: "Required",
+                minLength: "Must be at least 2 characters",
+                maxLength: "Must be 15 characters or less",
+              }}
             />
           </Col>
         </Row>
